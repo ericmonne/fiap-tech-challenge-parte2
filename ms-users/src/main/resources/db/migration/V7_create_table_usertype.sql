bdd90405eb6fbcb24fa519aa3758b545bb18511f
Vitor Fidelis <vitor.fidelis5587@gmail.com>
2025-07-23 12:00:12 -0300

    feat: adiciona script SQL para criação da tabela UserType

---
+++ ms-users/src/main/resources/db/migration/V7_create_table_usertype.sql
@@ -0,0 +1,5 @@
+CREATE TABLE usertype (
+                        id SERIAL PRIMARY KEY,
+                        typename VARCHAR(255) NOT NULL;
+                        description VARCHAR(255) NOT NULL
+);

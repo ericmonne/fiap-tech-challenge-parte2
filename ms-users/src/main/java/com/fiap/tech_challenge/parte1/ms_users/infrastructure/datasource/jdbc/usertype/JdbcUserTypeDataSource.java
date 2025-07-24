90fd594b023e80b48125caad074b3f33b068750c
Vitor Fidelis <vitor.fidelis5587@gmail.com>
2025-07-24 10:52:59 -0300

    feat(JdbcUserTypeEntity): adiciona a classe jdbcUserTypeEntity, cria seus metodos Getters e Setters, sem construtor

---
+++ ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/infrastructure/datasource/jdbc/usertype/JdbcUserTypeDataSource.java
@@ -0,0 +1,4 @@
+package com.fiap.tech_challenge.parte1.ms_users.infrastructure.datasource.jdbc.usertype;
+
+public class JdbcUserTypeDataSource {
+}
---
+++ ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/infrastructure/datasource/jdbc/usertype/JdbcUserTypeEntity.java
@@ -0,0 +1,57 @@
+package com.fiap.tech_challenge.parte1.ms_users.infrastructure.datasource.jdbc.usertype;
+
+import java.util.Date;
+
+public class JdbcUserTypeEntity {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean active;
+    private Date created;
+    private Date update;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(final Boolean active) {
+        this.active = active;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(final Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(final Date update) {
+        this.update = update;
+    }
+}

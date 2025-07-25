b84da846924f5c675ec4370e2c93ea86aa5f1b80
Vitor Fidelis <vitor.fidelis5587@gmail.com>
2025-07-25 09:14:27 -0300

    feat(UserTypeMapper): criação do arquivo UserTypeMapper, responsavel por conter metodos de conversão de tipos como entidade de modelo para entidade jdbc

---
+++ ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/application/port/output/usertype/UserTypeGateway.java
@@ -0,0 +1,21 @@
+package com.fiap.tech_challenge.parte1.ms_users.application.port.output.usertype;
+
+import com.fiap.tech_challenge.parte1.ms_users.domain.model.UserType;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserTypeGataway {
+
+    void createUserType(final UserType userType);
+
+    void deactivate(final Long id);
+
+    void reactivate(final Long id);
+
+    void update(final UserType userType);
+
+    List<UserType> findAll(final int size, int offset);
+
+    Optional<UserType> findById(final Long id);
+}
---
+++ ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/infrastructure/adapter/gateway/usertype/UserTypeGatewayImpl.java
@@ -0,0 +1,4 @@
+package com.fiap.tech_challenge.parte1.ms_users.infrastructure.adapter.gateway.usertype;
+
+public class UserTypeGatewayImpl {
+}

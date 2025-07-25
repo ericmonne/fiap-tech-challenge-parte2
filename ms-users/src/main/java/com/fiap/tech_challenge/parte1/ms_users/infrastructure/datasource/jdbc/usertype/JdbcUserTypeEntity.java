a79079ec0c4f79e253a4897e108368d9af575fc1
Vitor Fidelis <vitor.fidelis5587@gmail.com>
2025-07-25 09:28:51 -0300

    feat(jdbcUserTypeEntity): criação da entidade JDBC

--- ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/infrastructure/datasource/jdbc/usertype/JdbcUserTypeEntity.java
+++ ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/infrastructure/datasource/jdbc/usertype/JdbcUserTypeEntity.java
@@ -15,6 +15,10 @@ public class JdbcUserTypeEntity {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }

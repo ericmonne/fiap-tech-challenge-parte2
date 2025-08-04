e72ee2dbd8d0de3f1f06d92a298688c39bb3e817
Gabriel Soares <gabrielalcantarars@gmail.com>
2025-08-04 10:37:20 -0300

    fix: Correct SQL query for paginated menu items list

--- ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/infrastructure/datasource/jdbc/menu_item/JdbcMenuItemRepository.java
+++ ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/infrastructure/datasource/jdbc/menu_item/JdbcMenuItemRepository.java
@@ -48,7 +48,6 @@ public class JdbcMenuItemRepository {
                              image_path
                          FROM
                              menu_items
-                         ;
                  LIMIT :size
                  OFFSET :offset
                 """;
